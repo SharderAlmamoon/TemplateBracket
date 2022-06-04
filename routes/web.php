71b80a4a5969614a2225ea41d45b165be98dd58f
ALMAMOON <almamoon604@gmail.com>
2022-06-04 07:53:07 +0600

    Laravel breeze install Done

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -14,32 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('backend.dashboard');
-})->name('dashboard');
-
-
-
-// Route::group(['prefix'=>'product'],function(){
-//     Route::get('/store',function(){
-//         return view
-//     });
-// });
-Route::group(['prefix'=>'/product'],function(){
-    Route::post('/store','App\Http\Controllers\Backend\ProductController@store')->name('store');
-    Route::get('/create','App\Http\Controllers\Backend\ProductController@create')->name('create');
-    Route::get('/manage','App\Http\Controllers\Backend\ProductController@index')->name('manage');
-    Route::get('/edit/{id}','App\Http\Controllers\Backend\ProductController@edit')->name('edit');
-    Route::post('/update/{id}','App\Http\Controllers\Backend\ProductController@update')->name('update');
-    Route::get('/delete/{id}','App\Http\Controllers\Backend\ProductController@destroy')->name('delete');
-
+    return view('welcome');
 });
-// CATEGORY
-Route::group(['prefix'=>'/category'],function(){
-    Route::post('/catstore','App\Http\Controllers\Backend\CategoryController@store')->name('category.store');
-    Route::get('/forshow','App\Http\Controllers\Backend\CategoryController@show');
-    Route::get('/manage','App\Http\Controllers\Backend\CategoryController@index')->name('category.manage');
-    Route::get('/catedit/{id}','App\Http\Controllers\Backend\CategoryController@edit');
-    Route::post('/update/{id}','App\Http\Controllers\Backend\CategoryController@update');
-    Route::get('/delete/{id}','App\Http\Controllers\Backend\CategoryController@destroy');
 
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
