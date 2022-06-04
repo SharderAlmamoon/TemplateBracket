4dd90c0fd83f4a77c808a10eda8da7b1bd86772c
ALMAMOON <almamoon604@gmail.com>
2022-06-04 09:25:02 +0600

    all Route Secure Done

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/admin', function () {
     return view('backend.dashboard');
 })->middleware(['auth'])->name('dashboard');
 /*
@@ -25,25 +25,28 @@ Route::get('/dashboard', function () {
 | Our Custom Route
 |--------------------------------------------------------------------------
 */
-Route::group(['prefix'=>'/product'],function(){
-    Route::post('/store','App\Http\Controllers\Backend\ProductController@store')->name('store');
-    Route::get('/create','App\Http\Controllers\Backend\ProductController@create')->name('create');
-    Route::get('/manage','App\Http\Controllers\Backend\ProductController@index')->name('manage');
-    Route::get('/edit/{id}','App\Http\Controllers\Backend\ProductController@edit')->name('edit');
-    Route::post('/update/{id}','App\Http\Controllers\Backend\ProductController@update')->name('update');
-    Route::get('/delete/{id}','App\Http\Controllers\Backend\ProductController@destroy')->name('delete');
-
+Route::group(['/admin'=>'prefix'],function(){
+    Route::group(['prefix'=>'/product'],function(){
+        Route::post('/store','App\Http\Controllers\Backend\ProductController@store')->middleware(['auth'])->name('store');
+        Route::get('/create','App\Http\Controllers\Backend\ProductController@create')->middleware(['auth'])->name('create');
+        Route::get('/manage','App\Http\Controllers\Backend\ProductController@index')->middleware(['auth'])->name('manage');
+        Route::get('/edit/{id}','App\Http\Controllers\Backend\ProductController@edit')->middleware(['auth'])->name('edit');
+        Route::post('/update/{id}','App\Http\Controllers\Backend\ProductController@update')->middleware(['auth'])->name('update');
+        Route::get('/delete/{id}','App\Http\Controllers\Backend\ProductController@destroy')->middleware(['auth'])->name('delete');
+    
+    });
+    // CATEGORY
+    Route::group(['prefix'=>'/category'],function(){
+        Route::post('/catstore','App\Http\Controllers\Backend\CategoryController@store')->middleware(['auth'])->name('category.store');
+        Route::get('/forshow','App\Http\Controllers\Backend\CategoryController@show')->middleware(['auth']);
+        Route::get('/manage','App\Http\Controllers\Backend\CategoryController@index')->middleware(['auth'])->name('category.manage');
+        Route::get('/catedit/{id}','App\Http\Controllers\Backend\CategoryController@edit')->middleware(['auth']);
+        Route::post('/update/{id}','App\Http\Controllers\Backend\CategoryController@update')->middleware(['auth']);
+        Route::get('/delete/{id}','App\Http\Controllers\Backend\CategoryController@destroy')->middleware(['auth']);
+    
+    });
 });
-// CATEGORY
-Route::group(['prefix'=>'/category'],function(){
-    Route::post('/catstore','App\Http\Controllers\Backend\CategoryController@store')->name('category.store');
-    Route::get('/forshow','App\Http\Controllers\Backend\CategoryController@show');
-    Route::get('/manage','App\Http\Controllers\Backend\CategoryController@index')->name('category.manage');
-    Route::get('/catedit/{id}','App\Http\Controllers\Backend\CategoryController@edit');
-    Route::post('/update/{id}','App\Http\Controllers\Backend\CategoryController@update');
-    Route::get('/delete/{id}','App\Http\Controllers\Backend\CategoryController@destroy');
 
-});
 
 
 require __DIR__.'/auth.php';
